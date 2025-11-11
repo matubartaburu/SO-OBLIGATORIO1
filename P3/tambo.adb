66e8bab3fd53e921a28621f24daeb57fdef61d30
Mateo Bartaburu <matubartaburu2016@gmail.com>
2025-11-11 12:57:07 -0300

    Ej 2:Finalizado, Ej3:Encaminado

--- P3/tambo.adb
+++ P3/tambo.adb
@@ -1,94 +1,130 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
 
 procedure Tambo is
 
-task en_Ordeñe is 
-entry Entrar; 
-entry Salir;  -- Recursos compartidos.
-end en_Ordeñe;
+  task en_Ordene is
+    entry entrar;
+    entry salir;  -- Recursos compartidos.
+  end en_Ordene;
 
-task body en_Ordeñe is 
-En_Sala : Integer := 0; 
+  task body en_Ordene is
+    En_Sala : Integer := 0;
+  begin
+   loop 
+     select  
+      when en_Sala < 15  =>
+       accept entrar do
+        en_Sala := en_Sala + 1; 
+        end entrar; 
+      or 
+        accept salir do
+          En_Sala := En_Sala - 1; 
+        end salir; 
+      end select; 
+    end loop; 
+  end en_Ordene;
+
+  task en_Vacunacion is
+    entry entrar;
+    entry salir;
+  end en_Vacunacion;
+
+  task body en_Vacunacion is
+    Vacunandose : Integer := 0;
+  begin
+    loop
+      select
+        when Vacunandose < 5
+        =>accept entrar do
+          Vacunandose := Vacunandose + 1;
+        end entrar;
+      or
+        accept salir do
+        Vacunandose := Vacunandose - 1; 
+        end salir;
+      end select;
+    end loop;
+  end en_Vacunacion;
+
+  task Camion is
+    entry ingresar;
+  end Camion;
+
+  Camion1, Camion2 : Camion;
+
+  task body Camion is
+    En_Camion : Integer := 0;
+  begin
+    loop
+      select
+        when En_Camion < 50
+        =>accept ingresar do
+          en_Camion := en_Camion + 1;
+        end ingresar;
+      or
+        terminate;
+      end select;
+    end loop;
+  end Camion;
+
+  procedure Vacunacion (id : Integer) is
+  Gen: Ada.Numerics.Float_Random.Generator;
+begin
+    en_Vacunacion.entrar; --pide acceder. 
+     Put_Line("La vaca" & Integer'Image(ID) & " está entrando al área de vacunación");
+
+    delay Duration(Ada.Numerics.Float_Random.Random(Gen) * 2.0); --tiempo random en ser vacunada.
+
+    Put_Line("La vaca" & Integer'Image(ID) & " está saliendo del área de vacunación");
+
+   en_Vacunacion.Salir;
+end Vacunacion;
+
+
+  procedure Ordenar (id : Integer) is
+  Gen : Ada.Numerics.Float_Random.Generator; --la tengo que declarar cada vez que la uso. 
+  begin
+    en_Ordene.entrar; 
+    Put_Line("La vaca" & Integer'Image(ID) & " está entrando al área de ordeñe");
+    delay Duration(Ada.Numerics.Float_Random.Random(Gen) * 3.0);
+
+    Put_Line("La vaca" & Integer'Image(ID) & " está saliendo del área de ordeñe");
+    en_Ordene.salir; 
+  end Ordenar;
+
+  procedure subirCamion (id : Integer) is
+  begin
+   if id mod 2 = 0 then  --puede hacerse así ? o usamos gen? 
+    Camion1.ingresar;
+    Put_Line("La vaca" & Integer'Image(ID) & " está entrandó al camión 1");
+   else 
+    Camion2.ingresar; 
+    Put_Line("La vaca" & Integer'Image(ID) & " está entrandó al camión 2"); 
+   end if; 
+end subirCamion;
+     
+  task type Vaca (id : Integer);
+  task body Vaca is
+   Gen : Ada.Numerics.Float_Random.Generator;
+   R   : Float;
+  begin
+   Ada.Numerics.Float_Random.Reset(Gen);
+   R := Ada.Numerics.Float_Random.Random(Gen);
+   if R < 0.5 then  -- hacemos que varíen algunas el orden entre vacunarse y ordeñarse. 
+   Ordenar (id); 
+   Vacunacion (id); 
+   else
+    Vacunacion (id);
+    Ordenar(id);
+   
+   end if; 
+
+    SubirCamion (id); -- como viene despues del if me aseguro que sea despues de ambos. (letra)
+  end Vaca;
+
+  Vacas       : array (1 .. 100) of Vaca := (others => <>);
 begin
 
-end en_Ordeñe; 
-
- 
-
-task en_Vacunacion is 
-entry entrar; 
-entry salir; 
-end en_Vacunacion; 
-
-task body en_Vacunacion is 
-Vacunandose : Integer := 0; 
-begin
-   loop 
-    select  
-    when Vacunandose < 15 => accept entrar do 
-    Vacunandose := Vacunandose + 1; 
-end en_Vacunacion; 
-
-
-
-task Camion is
- entry Entrar;  
- entry Salir; 
-end Camion;
-Camion1, Camion2: Camion
-
-
-task body Camion is
- en_Camion : Integer:= 0
-begin 
-   loop 
-     select
-      when En_Camion < 50 => accept ingresar do 
-    en_Camion := en_Camion + 1; 
-  for I in 1.. 5 loop 
-Camion.ingresar; 
-    Put_Line("La vaca _id está entrando al camión");
-  end Ingresar; 
-    end select ; 
- end loop
-end Camion
-
-
-procedure Vacunacion(id: Integer) is 
-begin 
-  Area_Vacunacion.Entrar; 
-
-end Vacunacion; 
-
-procedure Ordeñar (id:Integer) is 
-begin 
-   en_Ordeñe.Entrar; 
-for I in 1 .. 5 loop 
-Put_Line("La vaca &id está entrando al area de ordeñe");
- end loop
-end Ordeñar; 
-
-procedure subirCamion(id: Integer) is
-begin  
-Camion.Entrar; 
-
-end subirCamion; 
-
-task type Vaca (id: Integer);
-task body Vaca is 
-begin 
-   Ordeñar(id);
-   Vacunacion(id);
-   --CAMION? 
-   SubirCamion(id); 
-
-
-end Vaca; 
-
-type Estado_Vaca is (En_Campo, En_Ordeñe, En_Vacunacion, En_Camion);
-Vaca_Estado : array (1 .. 100) of Estado_Vaca;
-Vacas : array (1..100) of Vaca := (others => <>); 
-
-end Tambo; 
+end Tambo;
--- p2/aeropuerto.c
+++ p2/aeropuerto.c
@@ -1,14 +1,15 @@
 #include <pthread.h>
 #include <stdio.h> 
 #include <stdlib.h>
+#include <semaphore.h>
 #include <unistd.h>
-pthread_mutex_t cartel; //recursocompartido.
+sem_t cartel; //recursocompartido.
 
 void* funcion_oficinista(void*arg); 
 void* funcion_pasajero(void*arg); 
 
 int main() {
-pthread_mutex_init(&cartel, NULL); //inicializo recurso ya creado. 
+sem_init(&cartel, 0, 1); //inicializo recurso ya creado. 
 pthread_t hilos_pasajeros[100]; 
 pthread_t hilos_oficinistas[5];  
 int Pasajeros[100]; //uso posiciones al imprimir.
@@ -17,46 +18,52 @@ for(int i = 0; i<5; i++) {
     Oficinistas[i] = i+1; 
     pthread_create(&hilos_oficinistas[i], NULL, funcion_oficinista, &Oficinistas[i]); 
 }
-for(int i = 0; i<100; i++) {
-    Pasajeros[i] = i +1; 
-    pthread_create(&hilos_pasajeros[i], NULL, funcion_pasajero, &Pasajeros[i]);    
+for (int i = 0; i < 50; i++) {
+        Pasajeros[i] = i + 1;
+        pthread_create(&hilos_pasajeros[i], NULL, funcion_pasajero, &Pasajeros[i]);
 }
-
-for(int i = 0; i<100; i++) { 
+for(int i = 0; i<50; i++) { 
 pthread_join(hilos_pasajeros[i], NULL); 
 } 
-
+for(int i = 50; i<100; i++) {
+    Pasajeros[i] = i+1; 
+    pthread_create(&hilos_pasajeros[i], NULL, funcion_pasajero, &Pasajeros[i]);
+}
+for (int i = 50; i < 100; i++) {
+        pthread_join(hilos_pasajeros[i], NULL); 
+}
+ 
 for(int i = 0; i<5; i++) {  
 pthread_join(hilos_oficinistas[i], NULL);
 
 }
 
-pthread_mutex_destroy(&cartel); //libero memoria.
+sem_destroy(&cartel); //libero memoria.
 
 return 0; 
 }
 
 void* funcion_pasajero(void*arg){ 
  //agrego espera de manera que sea más mezclado. 
-
 int id = *(int *)arg;
-pthread_mutex_lock(&cartel); 
-printf("Pasajero %d esta mirando el cartel\n", id);
 sleep(rand() % 3 + 1);
-pthread_mutex_unlock(&cartel);  
+sem_wait(&cartel); 
+printf("Pasajero %d esta mirando el cartel\n", id);
+sleep(rand() % 3 + 1); 
+sem_post(&cartel);  
+return NULL; 
 }; 
 
 void* funcion_oficinista(void*arg){ 
     int id = *(int* ) arg; 
 
     for(int i = 0; i<3 ;i++) {
-        pthread_mutex_lock(&cartel); 
+        sem_wait(&cartel); 
 
     printf("Oficinista %d esta modificando el cartel\n" , id);
     sleep(rand() % 5 + 1);
-    pthread_mutex_unlock(&cartel); 
-    sleep(rand() % 2 + 1);  
+    sem_post(&cartel);   
     }
+    return NULL; 
  
-
 }; 
